9699ebe4eb4941a4ee93469f52d3bdd3576c871d
kkismkn <kkismkn@gmail.com>
2019-08-10 23:23:43 +0900

    photo

--- main.py
+++ main.py
@@ -31,8 +31,6 @@ gauth = GoogleAuth()
 gauth.CommandLineAuth()
 drive = GoogleDrive(gauth)
 
-replyListText = ["写真を…写真をください…", "写真をくれればクラウドに保存するよ！", "なんや", "はろー", "会話は…ちょっと…", "わたしがくまだ", "はちみつください", "鮭とかくれてもいいよ"]
-
 # get channel_secret and channel_access_token from your environment variable
 channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
 channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
@@ -68,7 +66,9 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 #オウム返し
 def message_text(event):
-    replyText = random.choice(replyListText)
+    replyListText = ["写真を…写真をください…", "写真をくれればクラウドに保存するよ！", "なんや", "はろー", "会話は…ちょっと…",
+                     "わたしがくまだ", "はちみつください", "鮭とかくれてもいいよ"]
+    replyText = random.choice(replyList)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=replyText)
