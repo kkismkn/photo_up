0b02cd7d170613e934cac7849d6f98e5293e0949
kkismkn <kkismkn@gmail.com>
2019-08-12 17:45:59 +0900

    AI入れてみる

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ def message_text(event):
     params = {'apikey': "DZZiF7WacoaD6NVpsNl1bTJitbPQQC35",'query':event.message.text}
     r = requests.get(endpoint,params)
     data = json.loads(r.text)
-    reply_text = data['results'][0]['reply']
+    reply_text = data['status']
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=replyText)
