a6f38f1679fff3df0aec3be84bc0e0201990a185
kkismkn <kkismkn@gmail.com>
2020-01-17 23:43:41 +0900

    改行

--- main.py
+++ main.py
@@ -90,7 +90,9 @@ def message_text(event):
                      '【マイブーム】\n'
                      '　XXX\n'
                      '【マイブーム】\n'
-                     '　XXX\n')
+                     '　XXX\n'
+                     '【マイブーム】\n'
+                     '　XXX')
         line_bot_api.reply_message(event.reply_token, [image_message,text_message])
         return
     elif event.message.text == "新婦プロフィール":
